2f5869d86f0ebddd431a8be9cb31d35134816c59
Mathias Hermansson <mathias.hermansson@se.ibm.com>
2020-11-28 00:28:37 +0100

    Set branch name to master, due to bug in scoop's auto-pr
    This reverts commit 9e3767a9e40abdee60aba0ba730bb4c4a2f32889.

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "se35710/scoop-redhat:main"
+    [String]$upstream = "se35710/scoop-redhat:master"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
